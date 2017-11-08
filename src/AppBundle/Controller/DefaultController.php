d399c7aee1444642f23faf8e8b5933bf0bd37d83
yann clement <clementyann@free.fr>
2017-11-08 11:16:32 +0100

    ajout test controller code barre = OK

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use BG\BarcodeBundle\Util\Base1DBarcode as barCode;
+use BG\BarcodeBundle\Util\Base2DBarcode as matrixCode;
 
 class DefaultController extends Controller
 {
@@ -18,4 +20,18 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/bc", name="barcodetest")
+     */
+    public function bcAction(Request $request)
+    {
+        $myBarcode = new barCode();
+        $bcHTMLRaw = $myBarcode->getBarcodeHTML('501234567890', 'C128', 1.75, 45);
+        return $this->render('default/barcode.html.twig', array(
+
+            'barcodeHTML' => $bcHTMLRaw,
+        ));
+    }
+
 }
